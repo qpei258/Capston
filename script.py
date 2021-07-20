f238ae902e1638e1736fc3e22aada4f036dc90f7
qpei258 <81565492+qpei258@users.noreply.github.com>
2021-07-20 11:59:33 +0900

    Update script.py
    사진 촬영 완료

--- script.py
+++ script.py
@@ -1,23 +1,47 @@
 import tkinter as tk
 import time
 import cv2
+import os
 from PIL import ImageTk, Image
 
 #화면 갱신 함수
 def reset():
-    time_live = time.strftime("%H시 %M분 %S초")
-    date_live = time.strftime("%Y년 %m월 %d일")
+    time_live = time.strftime("%H? %M? %S?")
+    date_live = time.strftime("%Y? %m? %d?")
     timelabel.config(text=date_live+"\n\n"+time_live)
     timelabel.after(200, reset)
 
-#실시간 화면 송출 함수
+#실시간 화면 송출 및 사진 촬영 함수
 def video_play():
     ret, frame = cap.read()
     if not ret:
         cap.release()
         return
+    
+    #상하반전
+    frame = cv2.flip(frame, -1)
+    
+    
+    photo = frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(frame)
+    
+    #얼굴탐색
+    faces = faceCascade.detectMultiScale(
+        frame,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(20, 20)
+        )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = frame[y:y+h, x:x+h]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        cv2.imwrite('test.jpg', photo)
+    
+    
+    global img
+    img= Image.fromarray(frame)
     imgtk = ImageTk.PhotoImage(image=img)
 
     falselabel.imgtk = imgtk
@@ -25,13 +49,16 @@ def video_play():
     falselabel.after(10, video_play)
 
 
+
 win = tk.Tk()
-win.title("출입 시스템")
+win.title("?? ???")
 win.geometry("800x480")
-win.resizable(True, True)
+win.resizable(False, False)
+
+faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 #동영상 프레임
-cameraframe = tk.Frame(win, relief="groove", width=350, bg="white", bd=2)
+cameraframe = tk.Frame(win, relief="groove", width=350, height=300, bg="white", bd=2)
 cameraframe.pack(side="right", fill="both", expand=True)
 
 #텍스트 프레임
@@ -53,13 +80,13 @@ timelabel = tk.Label(dateframe, text="", font=("times", "14"))
 timelabel.pack(expand="True")
 
 
-#상테 메세지 라벨
-statelabel =  tk.Label(massegeframe, text="상태메세지")
+#상태 메시지 라벨
+statelabel =  tk.Label(massegeframe, text="?????")
 statelabel.pack(expand="True")
 
 
-#체온 라벨
-tampcurrent = tk.Label(tampframe, text="36.7°C", font=("times", "25"))
+#체온라벨
+tampcurrent = tk.Label(tampframe, text="36.7�C", font=("times", "25"))
 tampcurrent.pack(expand="True")
 
 #OpenCV->Tkinter변환용 라벨
@@ -72,7 +99,15 @@ cap.set(3, 300)
 
 
 video_play()
+if os.path.isfile('test.jpg'):
+    time.sleep(2)
+
 reset()
 
+
 win.mainloop()
+
+
+
+
 
