b638997b981f8e2172d23ae609e65bd1f7fe71b9
qpei258 <81565492+qpei258@users.noreply.github.com>
2021-08-11 15:38:41 +0900

    Add files via upload

---
+++ door_control.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+def control():
+    GPIO.setmode(GPIO.BCM)
+    door = 23
+
+    GPIO.setup(door, GPIO.OUT, initial=GPIO.LOW)
+
+    while True:
+        GPIO.output(door, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(door, GPIO.LOW)
+        time.sleep(0.5)
+
+    GPIO.cleanup()
